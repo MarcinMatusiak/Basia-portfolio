ea2a85f346438ab1330ae723fa7e352a3a0b0118
MarcinMatusiak <matusiakmr@gmail.com>
2020-10-16 12:45:07 +0200

    some cosmetics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import About from './About';
-import Contact from './Contact';
-import Home from './Home';
-import Prices from './Prices';
-import Categories from './Categories';
+import Navigation from './Navigation';
 import Resource from './Resource';
 import PATHS from '../paths';
 
@@ -23,21 +15,7 @@ class App extends React.Component {
   render () {
     return (
       <Router>
-        <nav>
-          <ul>
-            <li><Link to={PATHS.HOME}>Strona domowa</Link></li>
-            <li><Link to={PATHS.PORTFOLIO}>Portfolio</Link></li>
-            <Route path={PATHS.PORTFOLIO} component={Categories} />
-            <li><Link to={PATHS.PRICES}>Cennik</Link></li>
-            <li><Link to={PATHS.ABOUT}>O mnie</Link></li>
-            <li><Link to={PATHS.CONTACT}>Kontakt</Link></li>
-          </ul>
-
-          <Route exact path={PATHS.HOME} component={Home} />
-          <Route path={PATHS.PRICES} component={Prices} />
-          <Route path={PATHS.ABOUT} component={About} />
-          <Route path={PATHS.CONTACT} component={Contact} />
-        </nav>
+        <Navigation />
 
         <Route path={PATHS.RESOURCE} component={Resource} />
       </Router>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Prices from './Prices';
+import Categories from './Categories';
+import PATHS from '../paths';
+
+export default function Navigation () {
+  return (
+    <nav>
+      <ul>
+        <li><Link to={PATHS.HOME}>Strona domowa</Link></li>
+        <li><Link to={PATHS.PORTFOLIO}>Portfolio</Link></li>
+        <Route path={PATHS.PORTFOLIO} component={Categories} />
+        <li><Link to={PATHS.PRICES}>Cennik</Link></li>
+        <li><Link to={PATHS.ABOUT}>O mnie</Link></li>
+        <li><Link to={PATHS.CONTACT}>Kontakt</Link></li>
+      </ul>
+
+      <Route exact path={PATHS.HOME} component={Home} />
+      <Route path={PATHS.PRICES} component={Prices} />
+      <Route path={PATHS.ABOUT} component={About} />
+      <Route path={PATHS.CONTACT} component={Contact} />
+    </nav>
+  );
+};
