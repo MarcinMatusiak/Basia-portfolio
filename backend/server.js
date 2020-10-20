7d11b44e2b18cce0821c1a3bcee3a05dcec7c311
MarcinMatusiak <matusiakmr@gmail.com>
2020-10-20 13:45:22 +0200

    added sending emails

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,62 @@
-const Express = require('express');
-const App = Express();
-const BodyParser = require('body-parser');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const auth = require('./config/config');
+
+const router = express.Router();
+
+const App = express();
+App.use(cors());
+App.use(express.json());
+App.use('/', router);
+
 const PORT = 3000;
 
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+const transport = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: auth.USER,
+    pass: auth.PASS
+  }
+};
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const content = `name: ${name} \n email: ${email} \n message: ${message} `;
+
+  const mail = {
+    from: name,
+    to: `${auth.USER}`,
+    subject: 'Wiadomość z formularza kontaktowego',
+    text: content
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      });
+    } else {
+      res.json({
+        status: 'success'
+      });
+    }
+  });
+});
 
 App.get('/api', (req, res) => {
   res.send({ message: 'Seems to works' });
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,88 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
 
     this.state = {
-      value: ''
+      message: '',
+      name: '',
+      email: '',
+      phone: ''
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPhoneChange = this.onPhoneChange.bind(this);
+    this.onMessageChange = this.onMessageChange.bind(this);
   }
 
-  handleChange (event) {
-    const { value } = event.target;
-    this.setState(() => {
-      return {
-        value
-      };
+  handleSubmit (event) {
+    event.preventDefault();
+
+    fetch('http://localhost:3000/send', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(
+      (response) => (response.json())
+    ).then((response) => {
+      if (response.status === 'success') {
+        alert('Message Sent.');
+        this.resetForm();
+      } else if (response.status === 'fail') {
+        alert('Message failed to send.');
+      }
     });
+  }
+
+  onNameChange (event) {
+    this.setState({ name: event.target.value });
+  }
+
+  onEmailChange (event) {
+    this.setState({ email: event.target.value });
+  }
+
+  onPhoneChange (event) {
+    this.setState({ phone: event.target.value });
+  }
+
+  onMessageChange (event) {
+    this.setState({ message: event.target.value });
+  }
+
+  resetForm () {
+    this.setState({ name: '', email: '', message: '' });
   }
 
   render () {
     return (
-      <form>
-        <input
-          type='text'
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-      </form>
+      <div className='App'>
+        <form id='contact-form' onSubmit={this.handleSubmit} method='POST'>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input type='text' className='form-control' onChange={this.onNameChange} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email1'>Email address</label>
+            <input type='email' className='form-control' aria-describedby='email' onChange={this.onEmailChange} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='phone'>Phone number</label>
+            <input type='number' className='form-control' aria-describedby='phone' onChange={this.onPhoneChange} />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='message'>Message</label>
+            <textarea className='form-control' rows='5' onChange={this.onMessageChange} />
+          </div>
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+      </div>
     );
   }
 }
